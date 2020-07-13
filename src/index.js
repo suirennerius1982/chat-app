b8ffa662f864566ad81f35d5131d6e1651b65ed5
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-07-13 16:37:14 -0300

    my first socket example

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 
-console.log(__dirname)
-console.log(__filename)
+/* console.log(__dirname)
+console.log(__filename) */
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
-    console.log(`Server running in por ${port}`)
+io.on('connection', () => {
+    console.log('New web socket connection...')
+})
+
+server.listen(port, () => {
+    console.log(`Server running in port ${port}`)
 })
