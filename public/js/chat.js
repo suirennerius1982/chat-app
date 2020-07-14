e8dadce02d91bb2f1da44510fef213a706c531b0
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-07-14 13:47:19 -0300

    Example to use geolocation api to the browser, especific to chrome browser

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,3 +9,16 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     const message = e.target.elements.message.value
     socket.emit('sendMessage', message)
 })
+
+document.querySelector('#buttonGeo').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geo not disponible to this browser!!!')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -15,8 +15,13 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!!!')
     socket.broadcast.emit('message', 'A new user has joined...')
+    
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    })
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
     })
 
     socket.on('disconnect', () => {
