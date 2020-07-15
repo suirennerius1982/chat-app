a0a50fa4f699a82aa76811fd393abfaa18dd49b7
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-07-15 11:14:46 -0300

    Add behavior to the componets

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,24 @@
 const socket = io()
 
+const $formMessage = document.querySelector('#message-form')
+const $buttonSendMessage = $formMessage.querySelector('#buttonSendMenssage')
+const $inputMessage = $formMessage.querySelector('#message')
+const $buttonGeo = document.querySelector('#buttonGeo')
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$formMessage.addEventListener('submit', (e) => {
     e.preventDefault()
+    $buttonSendMessage.setAttribute('disabled', 'disabled')
     const message = e.target.elements.message.value
+
     socket.emit('sendMessage', message, (error) => {
+        $buttonSendMessage.removeAttribute('disabled')
+        $inputMessage.value = ''
+        $inputMessage.focus()
+
         if (error) {
             return console.log(error)
         }
@@ -15,16 +26,18 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     })
 })
 
-document.querySelector('#buttonGeo').addEventListener('click', () => {
+$buttonGeo.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geo not disponible to this browser!!!')
     }
 
+    $buttonGeo.setAttribute('disabled', 'disabled')
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, (message) => {
+            $buttonGeo.removeAttribute('disabled')
             console.log(`Location shared ${message}`)
         })
     })
