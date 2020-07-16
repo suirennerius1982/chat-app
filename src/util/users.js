686cb9e725359d0a972c6eb3b7cf2c9ca2f9a169
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-07-16 17:27:12 -0300

    Implemented the add and remove users

---
+++ src/util/users.js
@@ -0,0 +1,41 @@
+const users = []
+
+//adduser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    //Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    //Validate data
+    if(!username || !room) {
+        return {
+            error: 'User name and romm are requerided!'
+        }
+    }
+
+    //Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    //Validate username
+    if(existingUser) {
+        return {
+            error: 'This user name is in use!'
+        }
+    }
+
+    //Store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if(index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
