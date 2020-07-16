94466028d5a2d92a5d40e523cc0667d7bb15433c
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-07-16 16:10:30 -0300

    Begining use roon implementations

--- src/index.js
+++ src/index.js
@@ -15,16 +15,23 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
-    socket.emit('message', generateMessage('Welcome!!!'))
-    socket.broadcast.emit('message', generateMessage('A new user has joined...'))
-    
+
+    socket.on('join', ({ username, room} ) => {
+        socket.join(room)
+        socket.emit('message', generateMessage('Welcome!!!'))
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+
+        //socket.emit, io.emit (particular user), socket.broadcast.emit
+        //io.to.emit, socket.broadcast.to.emit
+    })
+     
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
         if (filter.isProfane(message)) {
             return callback(generateMessage('Profany is not allowed!'))
         }
         io.emit('message', generateMessage(message))
-        //calback('Delivered confirmation server')
+         //calback('Delivered confirmation server')
         callback()
     })
 
